edf08f3fccf3928f6f9db65a3a981779606f2a21
webtecham <67201665+webtecham@users.noreply.github.com>
2020-06-20 19:45:48 +0200

    Update newaccount.php

--- newaccount.php
+++ newaccount.php
@@ -1,14 +1,47 @@
 <!DOCTYPE HTML>
 <html>
-<head>
-<title> Studi-Maske </title>
-	<link rel="stylesheet" href="stylesheet.css">
-
-
-</head>
-
-<body>
-
+	<head>
+		<meta charset="utf-8">
+		<title> Studi-Maske </title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<!----Datenbank anbindung---->
+ 		<?php
+		    if(isset($_POST["submit"])){
+		      require("mysql.php");
+		      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user"); //Username überprüfen
+		      $stmt->bindParam(":user", $_POST["username"]);
+		      $stmt->execute();
+		      $count = $stmt->rowCount();
+		      if($count == 0){
+			//Username ist frei
+			$stmt = $mysql->prepare("SELECT * FROM accounts WHERE EMAIL = :email"); //Username überprüfen
+			$stmt->bindParam(":email", $_POST["email"]);
+			$stmt->execute();
+			$count = $stmt->rowCount();
+			if($count == 0){
+			  if($_POST["pw"] == $_POST["pw2"]){
+			    //User anlegen
+			    $stmt = $mysql->prepare("INSERT INTO accounts (USERNAME, PASSWORD, EMAIL, TOKEN) VALUES (:user, :pw, :email, null)");
+			    $stmt->bindParam(":user", $_POST["username"]);
+			    $hash = password_hash($_POST["pw"], PASSWORD_BCRYPT);
+			    $stmt->bindParam(":pw", $hash);
+			    $stmt->bindParam(":email", $_POST["email"]);
+			    $stmt->execute();
+			    echo "Dein Account wurde angelegt";
+			  } else {
+			    echo "Die Passwörter stimmen nicht überein";
+			  }
+			} else {
+			  echo "Email bereits vergeben";
+			}
+		      } else {
+			echo "Der Username ist bereits vergeben";
+		      }
+		    }
+     		?>
+		<!----Datenbank anbindung ENDE---->
 <!--Navbar -->
 <nav>
 	<ul>
@@ -22,30 +55,28 @@
 	
 	</ul>
 </nav>
-<!--Logininmaske-->
-<div class = "loginbox">
+
+
+<!--Loginmaske-->
+<div class = "Accounterstellenbox">
 	<img src="avatar.jpg" class="avatar">
-		<h1> Login </h1>
-
-			<form action ="auswertung.php" methode="POST">
+		<h1> Account erstellen </h1>
+		<form action="newaccount.php method="post">
+			<p>E-Mail Adresse</p>
+			<input type ="text" name="email" placeholder="E-Mail eingeben" required>
 			<p>Benutzername</p>
-			<input type ="text" name="userName" placeholder="Benutzernamen eingeben">
-			
-		
+			<input type ="text" name="username" placeholder="Benutzernamen eingeben" required>
 			<p>Passwort</p>
-			<input type="password" name="password" placeholder="Passwort eingeben"> 
-			
-			
-			<input type="submit" name="login" value="Login">
-			
-			
-			
-			<a href="Passw.html"> Passwort vergessen ? </a> <br>
-			<a href="Accounterstellen.html"> Account erstellen </a> 
-		
+			<input type="password" name="pw" placeholder="Passwort eingeben" required>
+			<p>Passwort</p>
+			<input type="password" name="pw2" placeholder="Passwort bestätigen" required>
+			<button type="submit" name="submit">Registrieren</button>
 		</form>
-
+<!--Loginmaske ENDE-->
+			
+		
 </div>
 
 </body>
+
 </html>
